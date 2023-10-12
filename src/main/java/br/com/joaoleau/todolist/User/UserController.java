32964534365e4191256fabb27e5af678397f0e68
Joao Leal <joaoo.lealc@gmail.com>
2023-10-11 21:05:18 -0300

    feat: Implement database and consolidate the API for 'todolist' backend

--- src/main/java/br/com/joaoleau/todolist/User/UserController.java
+++ src/main/java/br/com/joaoleau/todolist/User/UserController.java
@@ -1,5 +1,9 @@
 package br.com.joaoleau.todolist.User;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel usermodel) {
-        System.out.println(usermodel.name); // testando
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+        
+        if (user != null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario ja existe");
+        }
+        
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
